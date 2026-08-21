da5bde5934e46b1e55765b723beb18aa965444f2
Arcadia <Hernandesmarcos787@gmail.com>
2026-08-21 11:52:54 -0400

    add file

---
+++ script.js
@@ -0,0 +1,5 @@
+const password_regex = /^(?=.*[a-z])(?=.*[0-9]).{6,8}$/;
+const username_regex = /^(?=.*[a-z])(?=.*[0-9]).{6,8}$/;
+const email_regex = /^\s+@\s+\.\s+$/;
+const number_regex = /^[0-9]{6,16}$/;  
+
