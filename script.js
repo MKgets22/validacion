8b3597a1a85fccde7ff590e06a3664eeb31143e0
Arcadia <Hernandesmarcos787@gmail.com>
2026-08-24 20:30:05 -0400

    first commit

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ const password_regex = /^(?=.*[a-z])(?=.*[0-9]).{6,8}$/;
 const username_regex = /^(?=.*[a-z])(?=.*[0-9]).{6,8}$/;
 const email_regex = /^\s+@\s+\.\s+$/;
 const number_regex = /^[0-9]{6,16}$/;  
+ 
 
+
+//tarea diferencia entre sprin slice y String
